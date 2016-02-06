c671b23291aa7be81b3a7cf6902d5a971d365219
Marlan Perumal <marlan.perumal@gmail.com>
2016-02-06 22:19:32 +0200

    updated segmentation with latest mostly working method

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from viewer import viewer
 from aggregate import aggregateData
 from segmentation import process_image
 import skimage.io
+import matplotlib.pyplot as plt
+import numpy as np
 
 image = skimage.io.imread("Data/Unearthed Cape Town/De Beers Particle Size Challenge/ParticleSegmentationImages/original1.png")
 
 image, labelledFeat = process_image(image)
 colorData, sizeData = aggregateData(image,labelledFeat)
 
-view = viewer.viewerClass(image, labelledFeat, colorData, sizeData)
-view.view()
+fig, ax = plt.subplots(1,1)
+ax.imshow(labelledFeat, cmap=plt.cm.gray)
+plt.show()
+
+# view = viewer.viewerClass(image, labelledFeat, colorData, sizeData)
+# view.view()
--- segmentation.py
+++ segmentation.py
@@ -3,25 +3,40 @@ from skimage.io import imread
 import scipy.ndimage as ndi
 import matplotlib.pyplot as plt
 from skimage.color import label2rgb
+from skimage.exposure import rescale_intensity
+from skimage.filter import threshold_otsu as threshold_func
+from skimage.feature import peak_local_max
+from skimage.segmentation import find_boundaries
+from skimage import morphology
 
-def process_image(image_array):
-    image = imread("Data/Unearthed Cape Town/De Beers Particle Size Challenge/ParticleSegmentationImages/original1.png")
-    truth = imread("Data/Unearthed Cape Town/De Beers Particle Size Challenge/ParticleSegmentationImages/truth1.png")
-
+def process_image(image):
     # trim borders
     border_width = 50
     image = image[:, border_width:-border_width]
-    truth = truth[:, border_width:-border_width]
 
-    # filter truth image
-    truth2 = np.zeros_like(truth)
-    truth2[truth < 100] = 1
-    truth2[truth > 100] = 0
+    # rescale intensity
+    p2, p98 = np.percentile(image, (1, 99.9))
+    image = rescale_intensity(1.0*image, in_range=(p2, p98))
 
-    # label truth features
-    labeled_particles, num_features = ndi.label(truth2)
+    # do simple filter based on color value
+    thresh = 0.5*threshold_func(image)
+    filtered_image = np.zeros_like(image,dtype=np.uint8) # set up all-zero image
+    filtered_image[image > thresh] = 1 # filtered values set to 1
 
-    return image, labeled_particles
+    # # label truth features
+    # labeled_particles, num_features = ndi.label(truth2)
+    #
+    # # label features and convert to rgb image
+    # labeled_particles, num_features = ndi.label(filtered_image)
+    # image_label_overlay = label2rgb(labeled_particles, bg_label=0)
+    distance = ndi.distance_transform_edt(filtered_image)
+    local_maxi = peak_local_max(distance, indices=False, footprint=morphology.square(7),
+                            labels=filtered_image, exclude_border=False)
+    markers = ndi.label(local_maxi)[0]
+    labels = morphology.watershed(-distance, markers, mask=filtered_image)
+    labels[find_boundaries(labels)] = 0
+
+    return image, labels
 
 if __name__ == "__main__":
     image = imread("Data/Unearthed Cape Town/De Beers Particle Size Challenge/ParticleSegmentationImages/original1.png")
