60a61fbeff65a2c6befa398b759ffd6352abad31
matthew <dhsmat001@myuct.ac.za>
2016-02-06 14:08:42 +0200

    added aggregated data to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from viewer import viewer
+from aggregate import aggregateData
 import skimage.io
 
 image = skimage.io.imread("Data/Unearthed Cape Town/De Beers Particle Size Challenge/ParticleSegmentationImages/original1.png")
 
-viewer.view(image, image, None, None)
+colorData,SizeData = aggregateData(None,None)
+viewer.view(image, image, colorData, SizeData)
