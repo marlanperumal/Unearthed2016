6809f169f31f06e1d4d91075e7db7ce0d209939a
matthew <dhsmat001@myuct.ac.za>
2016-02-07 13:42:55 +0200

    Added viewing ability

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import skimage.io
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import time
 
 DIR = "Data/Unearthed Cape Town/De Beers Particle Size Challenge/Originals"
 for file in os.listdir(DIR):
@@ -16,4 +17,14 @@ for file in os.listdir(DIR):
         image, labelledFeat = process_image(image)
         colorData, sizeData = aggregateData(image,labelledFeat)
         view = viewer.viewerClass(image, labelledFeat, colorData, sizeData)
-        view.view()
+        view.view(selectSize=True, sizeValue=200, acont=True)
+
+
+image = skimage.io.imread("Data/Unearthed Cape Town/De Beers Particle Size Challenge/Originals/original1.png")
+image = trim_borders(image,150)
+image, labelledFeat = process_image(image)
+colorData, sizeData = aggregateData(image,labelledFeat)
+view = viewer.viewerClass(image, labelledFeat, colorData, sizeData)
+view.view(selectSize=True, sizeValue=200,acont=False)
+view.show()
+
